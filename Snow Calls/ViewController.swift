c883ab32facebc7aeb0e9916d7140e762e12392b
Branton Hung <BHung@lcs.on.ca>
2019-11-27 20:23:40 -0500

    Initial Commit

---
+++ Snow Calls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snow Calls
+//
+//  Created by Branton Hung on 2019-11-27.
+//  Copyright © 2019 Branton Hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
